14a84bb9b8ca0abcff2128944074047371231173
Abubakar Ibrahim <64968306+Abubakar-YI@users.noreply.github.com>
2020-06-16 23:49:54 +0100

    My Reads project

---
+++ precache-manifest.1f227172c2582f1f08be843730bcfdab.js
@@ -0,0 +1,38 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "0f5d3ec2ab1aed62b40537609f1262fd",
+    "url": "/my-reads/index.html"
+  },
+  {
+    "revision": "9d0dbfc37e4aaae28b90",
+    "url": "/my-reads/static/css/main.4ce52e51.chunk.css"
+  },
+  {
+    "revision": "3a4680761a9e9c6e0007",
+    "url": "/my-reads/static/js/2.1bddc8d2.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/my-reads/static/js/2.1bddc8d2.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "9d0dbfc37e4aaae28b90",
+    "url": "/my-reads/static/js/main.2a977a9b.chunk.js"
+  },
+  {
+    "revision": "0695e8140d665cd67e2b",
+    "url": "/my-reads/static/js/runtime-main.cb2c3f0a.js"
+  },
+  {
+    "revision": "bb9363b86b02777d23523851826414e0",
+    "url": "/my-reads/static/media/add.bb9363b8.svg"
+  },
+  {
+    "revision": "3687aa988bec28f17ae39153db1646c3",
+    "url": "/my-reads/static/media/arrow-back.3687aa98.svg"
+  },
+  {
+    "revision": "78c6f53b2342643639fe1cbaf171bf47",
+    "url": "/my-reads/static/media/arrow-drop-down.78c6f53b.svg"
+  }
+]);
